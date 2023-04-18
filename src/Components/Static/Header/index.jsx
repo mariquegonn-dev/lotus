fbaafca96f6ad57be5b8e4340548e4533f06a482
Rique Gonn <ocatodev@gmail.com>
2023-04-18 11:50:17 -0300

    feat: menu mobile and small changes

--- src/Components/Static/Header/index.jsx
+++ src/Components/Static/Header/index.jsx
@@ -1,23 +1,54 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Logo from "../../../assets/logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import useMedia from "../../../Hooks/useMedia";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const index = () => {
+  const mobile = useMedia("(max-width:40rem)");
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
-    <header className={styles.header}>
-      <NavLink to="/" end>
-        <img src={Logo} alt="" />
-      </NavLink>
-      <ul className={styles.nav}>
-        <li>
-          <NavLink to="caloriasDiarias">Calorias diárias</NavLink>
-        </li>
-        <li>
-          <NavLink to="macronutrientes">Macronutrientes</NavLink>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className={styles.header}>
+        <NavLink to="/" end>
+          <img src={Logo} alt="" />
+        </NavLink>
+        {mobile && (
+          <OutsideClickHandler onOutsideClick={() => setMobileMenu(false)}>
+            <button
+              className={`${styles.mobileButton} ${
+                mobileMenu && styles.active
+              }`}
+              aria-label="Menu"
+              onClick={(e) => {
+                e.preventDefault();
+                setMobileMenu(!mobileMenu);
+              }}
+            ></button>
+          </OutsideClickHandler>
+        )}
+        <ul
+          className={`${mobile ? styles.navMobile : styles.nav} ${
+            mobileMenu && styles.navMobileActive
+          }`}
+        >
+          <li>
+            <NavLink to="caloriasDiarias">Calorias diárias</NavLink>
+          </li>
+          <li>
+            <NavLink to="macronutrientes">Macronutrientes</NavLink>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 };
 
