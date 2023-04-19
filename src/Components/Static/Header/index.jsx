88868406bc7430b19e263accb0fb4994714c052a
Rique Gonn <ocatodev@gmail.com>
2023-04-19 14:29:08 -0300

    feat: dark mode initial

--- src/Components/Static/Header/index.jsx
+++ src/Components/Static/Header/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./styles.module.css";
-import Logo from "../../../assets/logo.svg";
 import { NavLink, useLocation } from "react-router-dom";
 import useMedia from "../../../Hooks/useMedia";
 import OutsideClickHandler from "react-outside-click-handler";
@@ -8,6 +7,8 @@ import OutsideClickHandler from "react-outside-click-handler";
 const index = () => {
   const mobile = useMedia("(max-width:40rem)");
   const [mobileMenu, setMobileMenu] = React.useState(false);
+  const [change, setChange] = React.useState(false);
+  const root = document.documentElement;
 
   const { pathname } = useLocation();
 
@@ -15,11 +16,19 @@ const index = () => {
     setMobileMenu(false);
   }, [pathname]);
 
+  React.useEffect(() => {
+    if (change) {
+      root.classList.add("active");
+    } else {
+      root.classList.remove("active");
+    }
+  }, [change]);
+
   return (
     <>
       <header className={styles.header}>
         <NavLink to="/" end>
-          <img src={Logo} alt="" />
+          <h1 className={styles.logo}>Lotus</h1>
         </NavLink>
         {mobile && (
           <OutsideClickHandler onOutsideClick={() => setMobileMenu(false)}>
@@ -45,6 +54,11 @@ const index = () => {
           </li>
           <li>
             <NavLink to="macronutrientes">Macronutrientes</NavLink>
+          </li>
+          <li>
+            <p onClick={() => setChange(!change)} className={styles.change}>
+              Change
+            </p>
           </li>
         </ul>
       </header>
